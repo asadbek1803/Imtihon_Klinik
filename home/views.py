c96aec4a5f6f9b2884ffbaf83c80b18a55b3bfb6
asadbek <asadbekabdubannopov0408@gmail.com>
2023-10-13 20:08:34 +0500

    Update: Add all pages in base view

--- home/views.py
+++ home/views.py
@@ -2,6 +2,40 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+
+
+def home(request): # Asosiy page uchun
 
     return render(request, "index.html")
+
+
+def Error404(request): # Mavjud emas Page
+    return render(request, "404.html")
+
+def about(reqeust): # About page
+
+    return render(reqeust, "about.html")
+
+def contact(reqeust): # Contact Page
+
+    return render(reqeust, "contact.html")
+
+def feature(request): # Feature Page
+
+    return render(request, "feature.html")
+
+def appointment(reqeust): # Appointment page
+
+    return render(reqeust, "appointment.html")
+
+def team(request): # Team page
+
+    return render(request, "team.html")
+
+def service(request): # Service page
+
+    return render(request, "service.html")
+
+def testimonial(request): # Testimonial page
+
+    return render(request, "testimonial.html")
